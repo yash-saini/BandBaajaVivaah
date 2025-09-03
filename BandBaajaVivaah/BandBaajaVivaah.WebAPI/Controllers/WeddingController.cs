f64ad9153cea94b5bd1b9c25794c079d6068e213
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-02 23:30:13 -0400

    Wedding Service added.

---
+++ BandBaajaVivaah/BandBaajaVivaah.WebAPI/Controllers/WeddingController.cs
@@ -0,0 +1,42 @@
+﻿using BandBaaajaVivaah.Data.Models;
+using BandBaajaVivaah.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandBaajaVivaah.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeddingController : ControllerBase
+    {
+        private readonly IWeddingService _weddingService;
+
+        public WeddingController(IWeddingService weddingService)
+        {
+            _weddingService = weddingService;
+        }
+
+        // GET: api/weddings/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWeddingById(int id)
+        {
+            var wedding = await _weddingService.GetWeddingByIdAsync(id);
+            if (wedding == null)
+            {
+                return NotFound();
+            }
+            return Ok(wedding);
+        }
+
+        // POST: api/weddings
+        [HttpPost]
+        public async Task<IActionResult> CreateWedding([FromBody] Wedding newWedding)
+        {
+            if (!ModelState.IsValid || newWedding == null)
+            {
+                return BadRequest(ModelState);
+            }
+            var createdWedding = await _weddingService.CreateWeddingAsync(newWedding);
+            return CreatedAtAction(nameof(GetWeddingById), new { id = createdWedding.WeddingId }, createdWedding);
+        }
+    }
+}
