869608cc8e9b907b0ae58f851b5bebcd2cb4d52b
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-06 19:38:52 -0400

    Guest repo added.

---
+++ BandBaajaVivaah/BandBaaajaVivaah.Data/Repositories/GuestRepository.cs
@@ -0,0 +1,21 @@
+﻿using BandBaaajaVivaah.Data.Models;
+using System.Linq.Expressions;
+
+namespace BandBaaajaVivaah.Data.Repositories
+{
+    public interface IGuestRepository : IRepository<Guest>
+    {
+        Task<IEnumerable<Guest>> FindAllAsync(Expression<Func<Guest, bool>> predicate);
+    }
+
+    public class GuestRepository : Repository<Guest>, IGuestRepository
+    {
+        public GuestRepository(BandBaajaVivaahDbContext context) : base(context)
+        {
+        }
+        public async Task<IEnumerable<Guest>> FindAllAsync(Expression<Func<Guest, bool>> predicate)
+        {
+            return await System.Threading.Tasks.Task.FromResult(_context.Guests.Where(predicate).ToList());
+        }
+    }
+}
---
+++ BandBaajaVivaah/BandBaajaVivaah.Contracts/DTO/GuestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BandBaajaVivaah.Contracts.DTOs;
+
+public class GuestDto
+{
+    public int GuestID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Side { get; set; }
+    public string RSVPStatus { get; set; }
+}
