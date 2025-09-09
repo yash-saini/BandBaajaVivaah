cb2d619e15a78a4e763e77e15634b51932de1dc1
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-09 18:27:41 -0400

    REgister user added.

--- BandBaajaVivaah/BandBaajaVivaah.WPF/Commands/RelayCommand.cs
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/Commands/RelayCommand.cs
@@ -4,6 +4,7 @@ namespace BandBaajaVivaah.WPF.Commands
 {
     public class RelayCommand : ICommand
     {
+        private readonly Action<object?>? _execute;
         private readonly Func<object, Task> _executeAsync;
         private readonly Predicate<object?> _canExecute;
         private bool _isExecuting;
@@ -11,6 +12,12 @@ namespace BandBaajaVivaah.WPF.Commands
         public RelayCommand(Func<object, Task> executeAsync, Predicate<object?> canExecute = null)
         {
             _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
@@ -36,7 +43,14 @@ namespace BandBaajaVivaah.WPF.Commands
                 // Raise the CanExecuteChanged event to disable the command while it's executing
                 CommandManager.InvalidateRequerySuggested();
 
-                await _executeAsync(parameter);
+                if (_executeAsync != null)
+                {
+                    await _executeAsync(parameter);
+                }
+                else
+                {
+                    _execute?.Invoke(parameter);
+                }
             }
             finally
             {
---
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,97 @@
+﻿using BandBaajaVivaah.Contracts.DTO;
+using BandBaajaVivaah.WPF.Commands;
+using BandBaajaVivaah.WPF.Services;
+using BandBaajaVivaah.WPF.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BandBaajaVivaah.WPF.ViewModel
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        private string _fullname;
+        public string FullName
+        {
+            get => _fullname;
+            set
+            {
+                _fullname = value;
+                OnPropertyChanged(nameof(FullName));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public ICommand RegisterCommand { get; }
+
+        private readonly ApiClientService _apiClient;
+        public RegisterViewModel(ApiClientService apiClient)
+        {
+            _apiClient = apiClient;
+            RegisterCommand = new RelayCommand(async _ => await RegisterAsync());
+        }
+
+        private async Task RegisterAsync()
+        {
+            var registerDto = new UserRegisterDto
+            {
+                FullName = this.FullName,
+                Email = this.Email,
+                Password = this.Password
+            };
+
+            var result = await _apiClient.RegisterAsync(registerDto);
+
+            switch (result)
+            {
+                case RegistrationResult.Success:
+                    Message = "Registration successful! You can now log in.";
+                    break;
+                case RegistrationResult.EmailAlreadyExists:
+                    Message = "This email address is already in use.";
+                    break;
+                case RegistrationResult.Failure:
+                    Message = "Registration failed. Please try again.";
+                    break;
+            }
+        }
+    }
+}
