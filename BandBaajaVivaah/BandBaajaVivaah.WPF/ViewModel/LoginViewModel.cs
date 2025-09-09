8f6a96bf625d147dfba7070ee71eaccb25ebc873
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-08 22:34:39 -0400

    Login Works.

---
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/ViewModel/LoginViewModel.cs
@@ -0,0 +1,95 @@
+﻿using BandBaajaVivaah.Contracts.DTO;
+using BandBaajaVivaah.WPF.Commands;
+using BandBaajaVivaah.WPF.Services;
+using BandBaajaVivaah.WPF.ViewModel.Base;
+using System.Windows.Input;
+
+namespace BandBaajaVivaah.WPF.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        private bool _isLoginSuccessful;
+        public bool IsLoginSuccessful
+        {
+            get => _isLoginSuccessful;
+            private set
+            {
+                _isLoginSuccessful = value;
+                OnPropertyChanged(nameof(IsLoginSuccessful));
+            }
+        }
+
+        private readonly ApiClientService _apiClient;
+
+        public LoginViewModel(ApiClientService apiClient)
+        {
+            _apiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+            LoginCommand = new RelayCommand(async _ => await LoginAsync(), _ => CanLogin());
+        }
+
+        private bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password);
+        }
+
+        private async Task LoginAsync()
+        {
+            try
+            {
+                ErrorMessage = string.Empty;
+                var loginDto = new UserLoginDto { Email = this.Email, Password = this.Password };
+                var token = await _apiClient.LoginAsync(loginDto);
+
+                if (token != null)
+                {
+                    IsLoginSuccessful = true;
+                }
+                else
+                {
+                    ErrorMessage = "Invalid email or password.";
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = $"Login failed: {ex.Message}";
+            }
+        }
+    }
+}
--- BandBaajaVivaah/BandBaajaVivaah.WPF/Views/LoginView.xaml.cs
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/Views/LoginView.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BandBaajaVivaah.WPF.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace BandBaajaVivaah.WPF.Views
 {
@@ -19,9 +8,11 @@ namespace BandBaajaVivaah.WPF.Views
     /// </summary>
     public partial class LoginView : Window
     {
+        private readonly LoginViewModel _viewModel;
         public LoginView()
         {
             InitializeComponent();
         }
     }
+    
 }
