6cde8e3e260f80ad6b7d13038ed8899eac77c408
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-09 23:35:11 -0400

    Email sending for forgot password.

---
+++ BandBaajaVivaah/BandBaajaVivaah.Contracts/DTO/ResetPasswordDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BandBaajaVivaah.Contracts.DTO
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
