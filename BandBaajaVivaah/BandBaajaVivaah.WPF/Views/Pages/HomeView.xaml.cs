0f9381514a30868740e97ae464483c34c559d363
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-13 17:08:52 -0400

    Added various pages and styled the application.

---
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/Views/Pages/HomeView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BandBaajaVivaah.WPF.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomeView.xaml
+    /// </summary>
+    public partial class HomeView : Page
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+            this.DataContext = Application.Current.MainWindow.DataContext;
+        }
+    }
+}
