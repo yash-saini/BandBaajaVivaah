92936c199f2571478b04ea0d62ddbd31676d0eac
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-09-28 01:33:22 -0400

    Guests Page added.

---
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/ViewModel/AddEditViewModel/AddEditGuestsViewModel.cs
@@ -0,0 +1,130 @@
+﻿using BandBaajaVivaah.Contracts.DTOs;
+using BandBaajaVivaah.WPF.Services;
+using BandBaajaVivaah.WPF.ViewModel.Base;
+using System.Windows;
+
+namespace BandBaajaVivaah.WPF.ViewModel.AddEditViewModel
+{
+    public class AddEditGuestsViewModel : ViewModelBase
+    {
+        private readonly ApiClientService _apiClient;
+        private readonly NavigationService _navigationService;
+        private readonly GuestDto? _editingGuests;
+        private readonly int _weddingId;
+
+        public string Title => _editingGuests == null ? "Add New Guest" : "Edit Guest";
+
+        private string _firstName = string.Empty;
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        private string _lastName = string.Empty;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        private string _side = string.Empty;
+        public string Side
+        {
+            get => _side;
+            set
+            {
+                _side = value;
+                OnPropertyChanged(nameof(Side));
+            }
+        }
+
+        private string _rsvpStatus = string.Empty;
+        public string RSVPStatus
+        {
+            get => _rsvpStatus;
+            set
+            {
+                _rsvpStatus = value;
+                OnPropertyChanged(nameof(RSVPStatus));
+            }
+        }
+
+        public bool WasSuccess { get; private set; } = false;
+
+        public event Func<Task> RefreshParentRequested;
+
+        public AddEditGuestsViewModel(ApiClientService apiClient, NavigationService navigationService, int weddingId, GuestDto? guest = null)
+        {
+            _apiClient = apiClient;
+            _navigationService = navigationService;
+            _editingGuests = guest;
+            _weddingId = weddingId;
+
+            if (_editingGuests != null)
+            {
+                // EDIT MODE
+                FirstName = _editingGuests.FirstName;
+                LastName = _editingGuests.LastName;
+                Side = _editingGuests.Side;
+                RSVPStatus = _editingGuests.RSVPStatus;
+            }
+            else
+            {
+                // ADD MODE
+                FirstName = string.Empty;
+                LastName = string.Empty;
+                Side = "Bride";
+                RSVPStatus = "No Response";
+            }
+        }
+
+        public void GoBack()
+        {
+            _navigationService.GoBack();
+        }
+
+        public async Task SaveAsync()
+        {
+            var dto = new CreateGuestDto
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Side = Side,
+                RSVPStatus = RSVPStatus,
+                WeddingID = _weddingId
+            };
+
+            bool success;
+            if (_editingGuests == null) // Add Mode
+            {
+                var newGuest = await _apiClient.CreateGuestAsync(dto);
+                success = newGuest != null;
+            }
+            else // Edit Mode
+            {
+                success = await _apiClient.UpdateGuestAsync(_editingGuests.GuestID, dto);
+            }
+
+            if (success)
+            {
+                WasSuccess = true;
+                await (RefreshParentRequested?.Invoke() ?? Task.CompletedTask);
+
+                _navigationService.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("Failed to save. Please try again.");
+            }
+        }
+    }
+}
--- BandBaajaVivaah/BandBaajaVivaah.WPF/Views/Pages/GuestsView.xaml.cs
+++ BandBaajaVivaah/BandBaajaVivaah.WPF/Views/Pages/GuestsView.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BandBaajaVivaah.WPF.Services;
+using BandBaajaVivaah.WPF.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NavigationService = BandBaajaVivaah.WPF.Services.NavigationService;
 
 namespace BandBaajaVivaah.WPF.Views.Pages
 {
@@ -20,9 +12,110 @@ namespace BandBaajaVivaah.WPF.Views.Pages
     /// </summary>
     public partial class GuestsView : Page
     {
-        public GuestsView()
+        GuestsViewModel? ViewModel => DataContext as GuestsViewModel;
+        private readonly ApiClientService _apiClient;
+        private readonly NavigationService _navigationService;
+        private readonly int _weddingId;
+
+        public GuestsView(ApiClientService apiClient, NavigationService navigationService, int weddingId)
         {
             InitializeComponent();
+            DataContext = new GuestsViewModel(apiClient, navigationService, weddingId);
+            _apiClient = apiClient;
+            _weddingId = weddingId;
+            _navigationService = navigationService;
+        }
+
+        private void Toolbar_BackButtonClicked(object sender, System.EventArgs e)
+        {
+            ViewModel?.GoBack();
+        }
+
+        private void Toolbar_AddButtonClicked(object sender, EventArgs e)
+        {
+            var formPage = new AddEditGuestsView(_apiClient, _navigationService, _weddingId);
+            _navigationService.NavigateTo(formPage);
+        }
+
+        private async void Toolbar_DeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (ViewModel != null)
+            {
+                await ViewModel.DeleteGuest();
+            }
+        }
+
+        private async void PageToolBarView_RefreshButtonClicked(object sender, EventArgs e)
+        {
+            if (ViewModel != null)
+            {
+                await ViewModel.LoadDataAsync();
+            }
+        }
+
+        private void DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (ViewModel?.SelectedItem != null)
+            {
+                var formPage = new AddEditGuestsView(_apiClient, _navigationService, _weddingId, ViewModel.SelectedItem);
+                _navigationService.NavigateTo(formPage);
+            }
+        }
+
+        private void Toolbar_FirstPageClicked(object sender, EventArgs e)
+        {
+            ViewModel?.GoToFirstPage();
+        }
+
+        private void Toolbar_PreviousPageClicked(object sender, EventArgs e)
+        {
+            ViewModel?.GoToPreviousPage();
+        }
+
+        private void Toolbar_NextPageClicked(object sender, EventArgs e)
+        {
+            ViewModel?.GoToNextPage();
+        }
+
+        private void Toolbar_LastPageClicked(object sender, EventArgs e)
+        {
+            ViewModel?.GoToLastPage();
+        }
+
+        private void DataGrid_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is DependencyObject source)
+            {
+                var row = FindParent<DataGridRow>(source);
+
+                // If the click was not on a row (e.g., on the empty space),
+                // row will be null.
+                if (row == null)
+                {
+                    if (sender is DataGrid grid)
+                    {
+                        // Clear the selection.
+                        grid.UnselectAll();
+                    }
+                }
+            }
+        }
+
+        private static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null) return null;
+
+            T? parent = parentObject as T;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                return FindParent<T>(parentObject);
+            }
         }
     }
 }
